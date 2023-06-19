a69b4a2e3958c0aa4c53045dbe2ed679939c1715
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-06-19 16:00:04 +0530

    Test2nd method TDD

--- testing/src/Components/greet/Greet.test.tsx
+++ testing/src/Components/greet/Greet.test.tsx
@@ -1,5 +1,7 @@
 //INTRODUCING OF TESTING
 
+
+
 // import { render,screen } from "@testing-library/react";
 // import  {Greet}  from "./greet";
 
@@ -9,5 +11,21 @@
 // expect(textElement).toBeInTheDocument();
 // })
 
+//Greet should render followed by by the hello and if the name is passed into the component.
+//It should render hello followed by the name
+//Test Driven Development
 //TDD APROACH
+import {render,screen} from "@testing-library/react";
+import {Greet} from "./greet";
 
+test('Greet renders correctly', () => {
+     render(<Greet />)
+      const textElement= screen.getByText("Hello") 
+ expect(textElement).toBeInTheDocument();
+})
+
+test('Greet renders with a name', () => {
+    render(<Greet name="Vishwas" />)
+    const textElement= screen.getByText("Hello Vishwas")
+expect(textElement).toBeInTheDocument();
+})
--- testing/src/Components/greet/Greet.tsx
+++ testing/src/Components/greet/Greet.tsx
@@ -6,3 +6,11 @@
 //     )
 //   }
   
+type GreetProps={
+  name?: string
+}
+export const Greet = (props: GreetProps) => {
+      return (
+        <div>Hello {props.name}</div>
+      )
+    }
